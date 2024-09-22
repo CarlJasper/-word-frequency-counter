59989c7dd7061adebe16421955becda2610312e3
cydie <cydricvincenttorillo04@gmail.com>
2024-09-22 23:37:03 +0800

    last commit

--- index.php
+++ index.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <title>Word Frequency Counter</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
-
 </head>
 <body>
     <h1>Word Frequency Counter</h1>
     
-    <form action="process.php" method="post">
+    <form action="" method="post">
         <label for="text">Paste your text here:</label><br>
         <textarea id="text" name="text" rows="10" cols="50" required></textarea><br><br>
         
@@ -24,5 +23,56 @@
         
         <input type="submit" value="Calculate Word Frequency">
     </form>
+
+    <?php
+    $stopWords = [
+        "the", "and", "in", "of", "a", "to", "is", "that", "it", "on", "for", "with", "as", "was", "were", "at", "by", 
+        "an", "be", "this", "which", "or", "from", "are", "but", "not", "can", "have", "has", "had", "will", "would", 
+        "could", "should"
+    ];
+
+    function calculateWordFrequency($text, $stopWords) {
+        $text = strtolower($text);
+        $text = preg_replace('/[^\w\s]/u', '', $text);
+        $words = preg_split('/\s+/', $text, -1, PREG_SPLIT_NO_EMPTY);
+        $filteredWords = array_filter($words, function ($word) use ($stopWords) {
+            return !in_array($word, $stopWords);
+        });
+        return array_count_values($filteredWords);
+    }
+
+    function sortWordFrequency($wordFrequency, $sortOrder) {
+        if ($sortOrder === 'asc') {
+            asort($wordFrequency);
+        } else {
+            arsort($wordFrequency);
+        }
+        return $wordFrequency;
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $text = $_POST['text'] ?? '';
+        $sortOrder = $_POST['sort'] ?? 'desc';
+        $limit = intval($_POST['limit']) ?? 10;
+
+        if (empty(trim($text))) {
+            echo "<h3>Error: Please provide some text for analysis.</h3>";
+            exit;
+        }
+
+        $wordFrequency = calculateWordFrequency($text, $stopWords);
+        $sortedWordFrequency = sortWordFrequency($wordFrequency, $sortOrder);
+        $limitedWordFrequency = array_slice($sortedWordFrequency, 0, $limit, true);
+
+        echo "<h2>Word Frequency Result</h2>";
+        echo "<table border='1' cellpadding='5' cellspacing='0'>";
+        echo "<thead><tr><th>Word</th><th>Frequency</th></tr></thead>";
+        echo "<tbody>";
+        foreach ($limitedWordFrequency as $word => $frequency) {
+            echo "<tr><td>{$word}</td><td>{$frequency}</td></tr>";
+        }
+        echo "</tbody></table>";
+    }
+    ?>
 </body>
 </html>
